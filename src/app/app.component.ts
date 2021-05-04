c61ef8261f0014480012b9090c341a89b0ad8679
Ayush Mishra <ayushmishra0810@gmail.com>
2021-05-05 01:31:40 +0530

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  signUpForm: FormGroup;
+  ngOnInit(): void {
+    this.signUpForm = new FormGroup({
+      club_name: new FormControl('', [Validators.required]),
+      club_logo: new FormControl('', [Validators.required]),
+      club_loaction: new FormGroup({
+        address: new FormControl('', [Validators.required]),
+        lat: new FormControl('', [Validators.required]),
+        log: new FormControl('', [Validators.required]),
+        country: new FormControl('', [Validators.required]),
+        city: new FormControl('', [Validators.required]),
+      }),
+      menu: new FormArray([]),
+      table_number: new FormControl('', [Validators.required]),
+      table_map_image: new FormControl('', [Validators.required]),
+      payment_method: new FormGroup({
+        card_number: new FormControl('', [
+          Validators.required,
+          Validators.maxLength(10),
+          Validators.minLength(10),
+        ]),
+        card_holder_name: new FormControl('', [Validators.required]),
+        bank_name: new FormControl('', [Validators.required]),
+        bank_location: new FormControl('', [Validators.required]),
+      }),
+      club_images: new FormArray([]),
+    });
+  }
   title = 'ClubSignUp';
+  addMenuCategory() {
+    (this.signUpForm.get('menu') as FormArray).push(
+      new FormGroup({
+        category_name:new FormControl('',[Validators.required]),
+       sub_category: new FormGroup({
+          name:new FormControl('',[Validators.required]),
+         items:new FormArray([])
+        })
+      })
+    );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
